7f7c08d149f7d98bf620d16a5c5967820e0907e5
redteampanda-ng <31235211+redteampanda-ng@users.noreply.github.com>
2023-03-07 08:17:20 +0100

    refactor: change extensions

--- conf.py
+++ conf.py
@@ -28,9 +28,9 @@ author = 'Florian Roth, Markus Meyer'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autosectionlabel'
-
+    'sphinx.ext.autosectionlabel',
+    'sphinx_rtd_theme',
+    'sphinx_search.extension'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
