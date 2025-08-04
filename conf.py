ba708a8656c4b04da5f2ba64336d638d68a4e831
redteampanda-ng <31235211+redteampanda-ng@users.noreply.github.com>
2025-08-04 09:05:41 +0200

    chore: update copyright year

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@ import os
 
 project = 'THOR Log Analysis'
 version="1.0"
-copyright = '2024, Nextron Systems GmbH'
+copyright = '2025, Nextron Systems GmbH'
 author = 'Nextron Systems'
 extensions = [
     'sphinx.ext.autosectionlabel',
