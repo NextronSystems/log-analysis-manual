9edf0ad35dd3582fae743beac507966a884e4cdf
redteampanda-ng <31235211+redteampanda-ng@users.noreply.github.com>
2026-02-05 15:18:05 +0100

    fix: medium link 403

--- conf.py
+++ conf.py
@@ -35,3 +35,6 @@ epub_exclude_files = ['search.html']
 intersphinx_mapping = {'https://docs.python.org/': None}
 # disable epub mimetype warnings
 suppress_warnings = ["epub.unknown_project_files"]
+linkcheck_ignore = [
+	r"^https://nasbench.medium.com/windows-system-processes-an-overview-for-blue-teams-42fa7a617920$",
+]
